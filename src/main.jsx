a6fddfec38827dae4dbeccf4d15176a6ea2bad3d
CodeAadarsh <aadarsh24lv@gmail.com>
2024-03-07 17:50:52 +0530

    404 page added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { Layout } from './Layout/Layout.jsx'
 import { Homepage } from './pages/Homepage/Homepage.jsx'
+import NotFound from './pages/404Page/NotFound.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Homepage />} />
       <Route path='about' element={<div>About</div>} />
+      <Route path='*' element={<NotFound/>} />
     </Route>,
 
 ))
