9f9c46a1fda975343bc9f5e0a4a62acc5295db0f
slaykalp <sankalp.agrawal2@s.amity.edu>
2024-04-05 15:27:56 +0530

    minor chnages

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -5,7 +5,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom';
 import LinkButtons from './LinkButtons';
 import TrainingCard from './TrainingCard';
-import HoveredLinks from './HoveredLinks';  
+import HoveredLinks from './HoveredLinks';
 import { Images } from '../assets';
 
 export default function DropDown() {
@@ -16,9 +16,9 @@ export default function DropDown() {
       <div>
         <Menu.Button onClick={() => setIsOpen(!isOpen)} className="inline-flex w-full justify-center rounded-md py-2  lg:font-bold  text-center">
           <div>
-            <LinkButtons links='Training' isOpen={isOpen}/>
+            <LinkButtons links='Training' isOpen={isOpen} />
           </div>
-          <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 ${isOpen? 'rotate-180':''} duration-300`} aria-hidden="true" />
+          <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 ${isOpen ? 'rotate-180' : ''} duration-300`} aria-hidden="true" />
         </Menu.Button>
       </div>
 
@@ -30,78 +30,78 @@ export default function DropDown() {
               <div className="lg:grid lg:grid-flow-row lg:grid-cols-3 lg:gap-10">
                 <Link className=''>
                   <div className='text-white lg:text-black lg:font-medium flex items-center '>
-                    <img src={Images.linux} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 "/>
+                    <img src={Images.linux} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 " />
                     Linux Fundamental
                     <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`} aria-hidden="true" />
                   </div>
                   <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>Essential skills for Linux operating system.</div>
                 </Link>
-                
+
                 <Link>
                   <div className='text-white lg:text-black lg:font-medium flex items-center '>
-                    <img src={Images.hacker} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 "/>
+                    <img src={Images.hacker} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 " />
                     Advanced Ethical Hacking
                     <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`} aria-hidden="true" />
                   </div>
                   <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'> Mastery in ethical hacking techniques.</div>
                 </Link>
-                
+
                 <Link>
                   <div className='text-white lg:text-black lg:font-medium flex items-center '>
-                    <img src={Images.webhack} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 "/>
-                    Web Application Penetration Testing 
+                    <img src={Images.webhack} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 " />
+                    Web Application Penetration Testing
                     <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`} aria-hidden="true" />
                   </div>
                   <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>Assessing security of web applications.</div>
                 </Link>
-                
+
                 <Link>
                   <div className='text-white lg:text-black lg:font-medium flex items-center '>
-                    <img src={Images.webbug} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 "/>
+                    <img src={Images.webbug} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 " />
                     Network Penetration Testing
                     <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`} aria-hidden="true" />
                   </div>
                   <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>Evaluating network security vulnerabilities rigorously.</div>
                 </Link>
-                
+
                 <Link className=''>
                   <div className='text-white lg:text-black lg:font-medium flex items-center '>
-                    <img src={Images.bugs} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 "/>
+                    <img src={Images.bugs} alt="Ethical Hacking" className="w-7 hidden lg:block mx-2 " />
                     Bug Bounty
-                    <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`}  aria-hidden="true" />
+                    <ChevronDownIcon className={`-mr-1 h-5 w-5 text-gray-400 -rotate-90`} aria-hidden="true" />
                   </div>
                   <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>Rewarding discovery of software vulnerabilities.</div>
                 </Link>
-                  
-                </div>
+
               </div>
             </div>
-            {/* new gray menu */}
-            <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
-              <Link>
+          </div>
+          {/* new gray menu */}
+          <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
+            <Link to='training/corporatetraining'>
               <p className='text-white lg:text-black font-bold text-lg lg:text-xl py-3'>Corporate training</p>
-              </Link>
-             
-             <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
-Tailored programs enhancing skills for organizational growth and efficiency.</div>
+            </Link>
+
+            <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
+              Tailored programs enhancing skills for organizational growth and efficiency.</div>
           </div>
-            {/* new gray menu */}
-            <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
-              <Link>
+          {/* new gray menu */}
+          <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
+            <Link to='training/diploma'>
               <p className='text-white lg:text-black font-bold text-lg lg:text-xl py-3'>Diploma in Cyber security</p>
-              </Link>
-             
-             <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
-             Comprehensive program securing digital environments with advanced strategies.</div>
+            </Link>
+
+            <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
+              Comprehensive program securing digital environments with advanced strategies.</div>
           </div>
-            {/* new gray menu */}
-            <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
-              <Link>
+          {/* new gray menu */}
+          <div className='lg:bg-slate-100 lg:my-1 lg:px-20 lg:h-36 flex flex-col justify-center'>
+            <Link to='training/internship'>
               <p className='text-white lg:text-black font-bold text-lg lg:text-xl py-3'>Internship training</p>
-              </Link>
-             
-             <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
-             Hands-on experience refining skills in professional work environments.</div>
+            </Link>
+
+            <div className='text-white lg:text-black lg:font-normal items-center hidden lg:block my-2'>
+              Hands-on experience refining skills in professional work environments.</div>
           </div>
         </div>
       </Menu.Items>
---
+++ src/pages/TrainingPage/CorporateTraining/CorporateTraining.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CorporateTraining = () => {
+  return (
+    <div>CorporateTraining</div>
+  )
+}
+
+export default CorporateTraining
