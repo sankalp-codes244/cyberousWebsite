d7033d5cc41e9819fe2c51f74370cffff931b670
slaykalp <sankalp.agrawal2@s.amity.edu>
2024-04-05 14:55:42 +0530

    made changes in career page

--- src/pages/Careers/Profiles/Profiles.jsx
+++ src/pages/Careers/Profiles/Profiles.jsx
@@ -26,14 +26,14 @@ const Profiles = () => {
   ]
   return (
     <div>
-      <div className='bg-custom-buttonColor-Green md:flex px-14 hidden mt-10'>
+      <div className='bg-custom-buttonColor-Green md:flex px-14 p-2 hidden '>
         <div className='w-1/2'>
-          <h1 className='text-xl md:text-3xl text-white font-Roboto'>
+          <h1 className='text-xl text-white font-Roboto'>
             Profile
           </h1>
         </div>
         <div className='w-1/2'>
-          <h1 className='text-xl md:text-3xl text-white font-Roboto'>
+          <h1 className='text-xl  text-white font-Roboto'>
             Description
           </h1>
         </div>
