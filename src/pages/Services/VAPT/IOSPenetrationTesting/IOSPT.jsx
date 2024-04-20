d4c3e4e4c6b3b60292b02c086e42440185ad4e38
slaykalp <sankalp.agrawal2@s.amity.edu>
2024-04-20 23:48:05 +0530

    Names you know Page added

--- src/pages/Services/VAPT/IOSPenetrationTesting/IOSPT.jsx
+++ src/pages/Services/VAPT/IOSPenetrationTesting/IOSPT.jsx
@@ -7,6 +7,7 @@ import BrandsTrustUs from '../../../Homepage/BrandsTrustUs/BrandsTrustUs'
 import Testemonial from '../../../Homepage/Testimonial/Testemonial'
 import Faqs from './FAQs/Faqs'
 import PageHeader from './PageHeader/PageHeader'
+import NamesYouKnow from '../../RedTeaming/NamesYouKnow/NamesYouKnow'
 
 const IOSPT = () => {
   const data = [
@@ -194,7 +195,9 @@ const IOSPT = () => {
       <Methodology boxData={boxData} />
       <EvaluateBusiness />
       <OurApproach approachData={approachData} />
-      <BrandsTrustUs />
+      {/* <BrandsTrustUs />
+       */}
+      <NamesYouKnow/>
       <Testemonial data={data}/>
       <Faqs faqData={faqData} />
     </div>
--- src/pages/Services/VAPT/SourceCode/SourceCode.jsx
+++ src/pages/Services/VAPT/SourceCode/SourceCode.jsx
@@ -7,6 +7,7 @@ import BrandsTrustUs from '../../../Homepage/BrandsTrustUs/BrandsTrustUs'
 import Testemonial from '../../../Homepage/Testimonial/Testemonial'
 import Faqs from './FAQs/Faqs'
 import PageHeader from './PageHeader/PageHeader'
+import NamesYouKnow from '../../RedTeaming/NamesYouKnow/NamesYouKnow'
 // import { Images } from '../../../assets'
 
 const SourceCode = () => {
@@ -193,7 +194,8 @@ const SourceCode = () => {
       <Methodology boxData={boxData} />
       <EvaluateBusiness />
       <OurApproach approachData={approachData} />
-      <BrandsTrustUs />
+      {/* <BrandsTrustUs /> */}
+      <NamesYouKnow/>
       <Testemonial data={data} />
       <Faqs faqData={faqData} />
     </div>
